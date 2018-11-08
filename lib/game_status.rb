c5a663a414a4b9bd120ad37bf5e49038a0c81b19
Dane Olsen <dno1995@gmail.com>
2018-11-08 14:41:13 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -12,11 +12,14 @@ WIN_COMBINATIONS = [
   [1,4,7],
   [2,5,8],
   [0,4,8],
-  [2,4,6]
-]
+  [2,4,6]]
 
 def won?(board)
   WIN_COMBINATIONS.detect do |win_combination|
     board[win_combination[0]] == board[win_combination[1]] && board[win_combination[1]] == board[win_combination[2]] && board[win_combination[0]] != ' '
   end
+end
+
+def full?(board)
+  board.all?{|index| index.all? position_taken(board, index)}
 end
